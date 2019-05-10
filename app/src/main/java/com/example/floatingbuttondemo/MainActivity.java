d43b30358a3342e9703c35ca87a13c6dfb9241df
Echo <echodsun@163.com>
2019-05-10 15:39:17 +0800

    添加悬浮按钮

--- app/src/main/java/com/example/floatingbuttondemo/MainActivity.java
+++ app/src/main/java/com/example/floatingbuttondemo/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.floatingbuttondemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FloatingActionButton mFabCallPolice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFabCallPolice = findViewById(R.id.fab_call_police);
+        mFabCallPolice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab_call_police:
+                Toast.makeText(this, "点击了按钮", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
