1f44b95769e627308723b59c849ff695a99ac68a
Echo <echodsun@163.com>
2019-05-11 09:33:45 +0800

    弹框

--- app/src/main/java/com/example/floatingbuttondemo/MainActivity.java
+++ app/src/main/java/com/example/floatingbuttondemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.floatingbuttondemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -25,6 +28,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.fab_call_police:
                 Toast.makeText(this, "点击了按钮", Toast.LENGTH_SHORT).show();
+
+                //https://blog.csdn.net/qq_35698774/article/details/79779238
+                final String[] items3 = new String[]{"拨打110", "拨打派出所值班室电话"};//创建item
+                AlertDialog alertDialog3 = new AlertDialog.Builder(this)
+                        .setTitle("选择您需要拨打的电话")
+                        .setIcon(R.mipmap.ivr)
+                        .setItems(items3, new DialogInterface.OnClickListener() {//添加列表
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(MainActivity.this, "点的是：" + items3[i], Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .create();
+                alertDialog3.show();
+                break;
+            default:
+
                 break;
         }
     }
